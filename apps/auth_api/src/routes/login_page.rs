a3c05cf41baeea972519e17b0ee5815fe63a1b18
Alogani <77880767+Alogani@users.noreply.github.com>
2026-01-30 18:58:28 +0000

    Add caching headers to login page response

--- apps/auth_api/src/routes/login_page.rs
+++ apps/auth_api/src/routes/login_page.rs
@@ -13,6 +13,8 @@ use tower_cookies::Cookies;
 
 use crate::templates::LoginTemplate;
 use auth::identify_user_with_cookie;
+use http::header::{CACHE_CONTROL, VARY};
+use http::HeaderValue;
 
 pub async fn login_page(
     cookies: Cookies,
@@ -36,7 +38,15 @@ pub async fn login_page(
     };
 
     match template.render() {
-        Ok(html) => Ok(Html(html).into_response()),
+        Ok(html) => {
+            let mut res = Html(html).into_response();
+            res.headers_mut().insert(
+                CACHE_CONTROL,
+                HeaderValue::from_static("no-store, no-cache, must-revalidate"),
+            );
+            res.headers_mut().insert(VARY, HeaderValue::from_static("Cookie"));
+            Ok(res)
+        }
         Err(err) => {
             tracing::error!("Template error: {}", err);
             Ok(StatusCode::INTERNAL_SERVER_ERROR.into_response())
